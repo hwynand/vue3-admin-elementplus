821976d79387990253d7ec94a66edd05988c4611
huongpx <huong.px01@gmail.com>
2023-04-03 00:13:41 +0700

    feat(product): filter by categories

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { ElMessage, ElMessageBox } from 'element-plus'
 
@@ -10,6 +10,13 @@ const productStore = useProductsStore()
 const router = useRouter()
 const getProductListParams: IGetProductListParams = reactive({})
 productStore.getProductList(getProductListParams)
+
+const categories = ref([])
+productStore.getCategoryList()
+function handleChangeCategory(newCategories: number[]) {
+  getProductListParams.category_id = newCategories
+  productStore.getProductList(getProductListParams)
+}
 
 function handleEdit(id: number) {
   router.push('/products/' + id)
@@ -45,6 +52,12 @@ function changePage(page: number) {
 
 <template>
   <div>
+    <div class="filter-options">
+      <el-select v-model="categories" @change="handleChangeCategory" multiple placeholder="Category" size="large">
+        <el-option v-for="category in productStore.categoryList" :key="category.id" :label="category.name"
+          :value="category.id" />
+      </el-select>
+    </div>
     <el-text class="mx-1" size="large">{{ productStore.totalProduct }} products</el-text>
     <el-table :data="productStore.productList" stripe>
       <el-table-column prop="id" label="ID" width="50" />
@@ -63,19 +76,13 @@ function changePage(page: number) {
       </el-table-column>
       <el-table-column label="Actions" width="180">
         <template #default="scope">
-          <el-button size="small" @click="handleEdit(scope.row.id)"
-            >Edit</el-button
-          >
-          <el-button
-            size="small"
-            type="danger"
-            @click="handleDelete(scope.row.id)"
-            >Delete</el-button
-          >
+          <el-button size="small" @click="handleEdit(scope.row.id)">Edit</el-button>
+          <el-button size="small" type="danger" @click="handleDelete(scope.row.id)">Delete</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination background layout="prev, pager, next" :total="productStore.totalProduct" @current-change="changePage" />
+    <el-pagination background layout="prev, pager, next" :total="productStore.totalProduct"
+      @current-change="changePage" />
   </div>
 </template>
 
@@ -83,5 +90,9 @@ function changePage(page: number) {
 .el-pagination {
   justify-content: end;
   margin: 15px;
+}
+
+.filter-options {
+  margin-bottom: 10px;
 }
 </style>
--- src/stores/product.ts
+++ src/stores/product.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 
-import type { IProduct } from '@/interfaces'
+import type { IProduct, ICategory } from '@/interfaces'
 import { api } from '@/utils'
 import type { IGetProductListParams } from '@/interfaces'
 
@@ -8,6 +8,7 @@ export const useProductsStore = defineStore('products', {
   state: () => ({
     totalProduct: null as number | null,
     productList: [] as IProduct[],
+    categoryList: [] as ICategory[],
   }),
   actions: {
     async getProductList(params: IGetProductListParams) {
@@ -24,6 +25,14 @@ export const useProductsStore = defineStore('products', {
         const { data } = await api.delete<IProduct>('/products/' + id)
       } catch (error) {
         console.error('Delete Product E: ', error)
+      }
+    },
+    async getCategoryList() {
+      try {
+        const { data } = await api.get('/categories/')
+        this.categoryList = data
+      } catch (error) {
+        console.error('Category store E: ', error)
       }
     }
   }
