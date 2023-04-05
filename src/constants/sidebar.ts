e6adf1238b773e09b3b382dc66751af7adb84221
huongpx <huong.px01@gmail.com>
2023-04-05 10:43:00 +0700

    update sidebar: using iconify sets

--- src/constants/sidebar.ts
+++ src/constants/sidebar.ts
@@ -1,40 +1,31 @@
-import {
-  Menu as IconMenu,
-  DataAnalysis,
-  User,
-  Reading,
-  Cellphone,
-  ShoppingCart,
-} from '@element-plus/icons-vue'
-
 export const MENU = [
   {
     index: "1",
-    icon: DataAnalysis,
+    icon: "material-symbols:dashboard",
     to: '/dashboard',
     name: 'Dashboard'
   },
   {
     index: "2",
-    icon: User,
+    icon: "ph:user-list-fill",
     to: '/users',
     name: 'Users'
   },
   {
     index: "3",
-    icon: Cellphone,
+    icon: "dashicons:products",
     to: '/products',
     name: 'Products'
   },
   {
     index: "4",
-    icon: ShoppingCart,
+    icon: "material-symbols:order-approve",
     to: '/orders',
     name: 'Orders'
   },
   {
     index: "5",
-    icon: Reading,
+    icon: "material-symbols:article",
     to: '/articles',
     name: 'Article'
   },
